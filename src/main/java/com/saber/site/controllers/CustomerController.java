b0db1bc248a4d629da64ac28bb47524e4310e8b8
saberlee1987 <saberazizi66@yahoo.com>
2021-06-05 04:45:57 +0430

    add Save and show list customer page

--- src/main/java/com/saber/site/controllers/CustomerController.java
+++ src/main/java/com/saber/site/controllers/CustomerController.java
@@ -1,5 +1,6 @@
 package com.saber.site.controllers;
 
+import com.saber.site.dto.CustomerDto;
 import com.saber.site.entities.CustomerEntity;
 import com.saber.site.services.CustomerService;
 import org.slf4j.Logger;
@@ -7,7 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -31,5 +34,20 @@ public class CustomerController {
         System.out.println("customers ===> "+customerEntityList);
         model.addAttribute("customers",customerEntityList);
         return "customer/list";
+    }
+    @GetMapping(value = "/save")
+    public String addCustomer(Model model){
+        model.addAttribute("customer",new CustomerDto());
+        return "customer/save";
+    }
+    @PostMapping(value = "/save")
+    public RedirectView saveCustomer(CustomerDto customerDto){
+        CustomerEntity customerEntity = this.customerService.saveCustomer(customerDto);
+        if (customerEntity!=null){
+            return new RedirectView("/customer/list",true,false);
+        }else{
+            return new RedirectView("/customer/error",true,false);
+        }
+
     }
 }
